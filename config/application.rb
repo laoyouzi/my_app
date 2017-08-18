6e273c1dc5eb1843d7413757f48e044d2a19b739
youzi <xiaoming.wang@alo7.com>
2017-08-18 22:08:22 +0800

    jquery plugin

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module MyApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+    config.autoload_paths << Rails.root.join('lib')
 
 
     config.generators do |g|
---
+++ vendor/assets/javascripts/app.modal.js
@@ -0,0 +1,25 @@
+;(function() {
+  
+  var openModal = function(tooltip){
+
+    var dialog = $(tooltip).appendTo(document.body);
+    return dialog
+  }
+
+  var closeModal = function(){
+
+  }
+
+  $.modalAlert = function(content) {
+    var tooltip = ''
+
+    //var dialog = openModal(tooltip)
+    //$().on('click', '') {
+    //  closeModal()
+    //}
+    console.log("test")
+    alert("heloo")
+
+  }
+
+})(jQuery);
